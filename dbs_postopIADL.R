34c9b4cba3677d68f6f2415b92415203876d28b2
josefmana <josef.mana@protonmail.com>
2022-10-22 21:28:04 +0200

    modelling, ppc, and posterior parameters

--- dbs_postopIADL.R
+++ dbs_postopIADL.R
@@ -9,8 +9,8 @@ setwd( dirname(rstudioapi::getSourceEditorContext()$path) )
 # list required packages into a character object
 pkgs <- c(
   "dplyr", "tidyverse", # for data wrangling
-  "brms", "tidybayes", # for Bayesian analyses 
-  "ggplot2", "ggpubr" # for plotting
+  "brms", "tidybayes", "bayestestR", # for Bayesian analyses 
+  "ggplot2", "patchwork" # for plotting
 )
 
 # load or install packages as needed
@@ -35,7 +35,7 @@ s = 87542 # seed for reproducibility
 sapply( c("models", "figures", "tables", "sessions"), function(i) if( !dir.exists(i) ) dir.create(i) )
 
 # set ggplot theme
-theme_set( theme_classic(base_size = 14) )
+theme_set( bayesplot::theme_default( base_size = 14 ) )
 
 # prepare colors to use in graphs (a colorblind-friendly palette)
 cbPal <- c( "#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7" )
@@ -48,7 +48,7 @@ d.nms <- read.csv( "data/item_nms.csv" , sep = "," , row.names = 1 ) # item name
 
 # ----------- heuristic causal model (DAG)  -----------
 
-# will be created via DiagrammeR
+# will be created via ggdag
 # for now, I know I need to adjust for preop. DRS2, BDI-II and LEDD for total effect
 # and adjust for postop. DRS-2, BDI-II and LEDD for direct effect
 
@@ -61,22 +61,18 @@ d1 <- d0 %>% select( id, assessment_type, levodopa_equivalent, drsii_total, bdi,
                      starts_with("pdaq") # outcome
                      ) %>%
   # rename the variables so that I don't go crazy
-  rename( ass_type = assessment_type, led = levodopa_equivalent, drs = drsii_total ) %>%
-  # change "post_1y" to "post" because they are all one-year after DBS operation
-  mutate( ass_type = ifelse( ass_type == "post_1y", "1_post", "0_pre" ) %>% as.factor() )
+  rename( post = assessment_type, led = levodopa_equivalent, drs = drsii_total ) %>%
+  mutate( post = ifelse( post == "post_1y", 1, 0 ) %>% as.factor() ) # re-code post-surgery indicator variable
 
 # check there ain't no NAs in the data set
 sapply( names(d1) , function(i) sum( is.na(d1[[i]]) ) ) # seems ok
 all( sapply( names(d1) , function(i) sum( is.na(d1[[i]]) ) ) == 0 ) # indeed, it is ok, no NAs
 
-# extract the number of patients
-N = length( unique(d1$id) ) # N = 32
-
 # find a reasonable scaling of predictors
 # first get an idea about in-sample variability
-sapply( levels(d1$ass_type) , function(i) # loop through pre/post assessments
+sapply( levels(d1$post) , function(i) # loop through pre/post assessments
   sapply( c("drs","bdi","led") , function(j) # loop through all predictors
-    round( sd( d1[[j]][ d1$ass_type == i ] , na.rm = T ) , 2 )
+    round( sd( d1[[j]][ d1$post == i ] , na.rm = T ) , 2 )
     )
   )
 
@@ -85,9 +81,9 @@ sapply( levels(d1$ass_type) , function(i) # loop through pre/post assessments
 scl <- data.frame(
   # first create a column of means to use for scaling
   M = c(
-    mean( d1$drs[d1$ass_type == "0_pre"] , na.rm = T ),
-    mean( d1$bdi[d1$ass_type == "0_pre"] , na.rm = T ),
-    mean( d1$led[d1$ass_type == "0_pre"] , na.rm = T )
+    mean( d1$drs[d1$post == 0] , na.rm = T ),
+    mean( d1$bdi[d1$post == 0] , na.rm = T ),
+    mean( d1$led[d1$post == 0] , na.rm = T )
   ),
   # then add user(me)-specified scaling factors based on theoretical consideration and data variability
   SD = c(
@@ -114,7 +110,7 @@ length( unique(d2$item) )
 max( d2$item ) # A-OK
 
 # check that patient/item/assessment type each combination is there exactly once
-table( d2$id , d2$item , d2$ass_type ) # A-OK
+table( d2$id , d2$item , d2$post ) # A-OK
 
 # scale DRS and LEDD such that the model samples more efficiently
 # make the outcome ordinal such that brms can take it in
@@ -122,21 +118,28 @@ df <- d2 %>%
   mutate( drs = ( drs - scl["drs","M"] ) / scl["drs","SD"],
           bdi = ( bdi - scl["bdi","M"] ) / scl["bdi","SD"],
           led = ( led - scl["led","M"] ) / scl["led","SD"],
-          resp = as.ordered( resp ), item = paste0("i",item)
+          resp = as.ordered( resp )
           )
 
 # check the contrast for pre/post factor
-contrasts( df$ass_type ) # ok, it's a dummy coding as I wanted
+contrasts( df$post ) # ok, it's an indicator variable coded as I wanted
+
+# check the number of patients
+N <- length( unique(df$id) ) # N = 32
 
 
 # ---- models set-up ---- 
 
-# setp-up linear models
+# set-up linear models
+# m0 = total effect of time ("main effect") (the masked model)
+# m1 = total effect of LEDD ("total effect") (the primary model)
+# m2 = direct effect of LEDD ("direct effect") (the control model)
+# m3 = direct effect of LEDD ("original model") (the original logit model presented in the article)
 f <- list(
-  orig = bf( resp ~ ass_type * led + ass_type * drs + ass_type * bdi + ( 1 | id ) + ( 1 | item ) ) + cumulative("logit"),
-  prob = bf( resp ~ ass_type * led + ass_type * drs + ass_type * bdi + ( 1 | id ) + ( 1 | item ) ) + cumulative("probit"),
-  ledd = bf( resp ~ ass_type * led + ( 1 | id ) + ( 1 | item ) ) + cumulative("probit")#,
-  #flex = bf( resp | thres(gr = item) ~ ass_type * led + ( 1 | id ) + ( 1 | item ) ) + cumulative("probit")
+  m0_meff = bf( resp ~ 1 + post + ( 1 | id ) + ( 1 | item ) ) + cumulative("probit"),
+  m1_teff = bf( resp ~ 1 + post * led + ( 1 | id ) + ( 1 | item ) ) + cumulative("probit"),
+  m2_deff = bf( resp ~ 1 + post * led + post * drs + post * bdi + ( 1 | id ) + ( 1 | item ) ) + cumulative("probit"),
+  m3_orig = bf( resp ~ 1 + post * led + post * drs + post * bdi + ( 1 | id ) + ( 1 | item ) ) + cumulative("logit")
 )
 
 # set-up priors (the same for all models)
@@ -159,3 +162,192 @@ for ( i in names(f) ) m[[i]] <- brm( formula = f[[i]], prior = p,
                                      file = paste0( "models/",i,".rds" ),
                                      save_model = paste0("models/",i,".stan")
                                      )
+
+
+# ----------- soft model checking -----------
+
+# check the highest Rhat for chains convergence and Pareto-k for influential outliers
+cbind.data.frame(
+  R_hat = sapply( names(m) , function(i) max( rhat(m[[i]]), na.rm = T ) ) %>% round(3),
+  Pareto_k = sapply( names(m) , function(i) max( loo(m[[i]])$diagnostics$pareto_k, na.rm = T ) ) %>% round(3)
+)
+
+# check Pareto-k visually as well
+par( mfrow = c(2,2) )
+for ( i in names(m) ) plot( loo(m[[i]]), main = i )
+par( mfrow = c(1,1) )
+
+# plot posterior predictive check stratified by item
+ppc <- list()
+
+# plotting proper
+for ( i in names(m) ) {
+  
+  # set distinct color palettes for different models
+  if( i == "m0_meff") bayesplot::color_scheme_set( scheme = "red" )
+  else if ( i == "m1_teff" ) bayesplot::color_scheme_set( scheme = "orange" )
+  else if ( i == "m2_deff" ) bayesplot::color_scheme_set( scheme = "yellow" )
+  else if ( i == "m3_orig" ) bayesplot::color_scheme_set( scheme = "green" )
+  
+  # plot the per item posterior predictive check
+  ppc[[i]] <- pp_check( m[[i]], type = "bars_grouped", ndraws = NULL, group = "item" ) +
+    theme( legend.position = "none" )
+  
+}
+
+# plot them together
+( ( ppc$m0_meff + labs(title = "Ordered-probit (main effect)") ) | ( ppc$m1_teff + labs( title = "Ordered-probit (total effect)" ) ) ) /
+  ( ( ppc$m2_deff + labs( title = "Ordered-probit (direct effect)" ) ) | ( ppc$m3_orig + labs( title = "Ordered-logit (direct effect)" ) ) )
+
+# save it
+ggsave( "figures/per item posterior predictive checks.jpg", width = 1.5*9.62, height = 1.2*11.7, dpi = 600 )
+
+
+# ----------- posterior predictive check (item/session) -----------
+
+# in this section I extract ppc for m1 only
+# extract posterior retrodictions in the form of predicted response for each data row in each MCMC iteration
+ppred <- predict( m$m1_teff, summary = F )
+
+# prepare a table containing each item/session combination
+d_seq <- expand.grid( unique(df$item), unique(df$post) ) %>% `colnames<-`( c("item","post") ) %>%
+  # add labels of both items and sessions
+  mutate( Item = factor( rep( d.nms$name , 2 ), levels = d.nms$name , ordered = T ),
+          Time = factor( ifelse( post == 0 , "pre-surgery", "post-surgery"),
+                         levels = paste0(c("pre","post"),"-surgery"), ordered = T )
+          )
+
+# add observed frequencies
+for ( i in 0:4 ) {
+  # prepare a column for each response type
+  d_seq[[paste0("resp_",i)]] <- NA
+  # loop through all item/session combinations to fill-in observed frequencies of each response 0:4
+  for( j in 1:nrow(d_seq) ) d_seq[ j, paste0("resp_",i) ] <- ( with( df, ( resp[ which( item == d_seq$item[j] & post == d_seq$post[j] ) ] ) ) == i ) %>% sum()
+}
+
+# change the table to a long format
+d_seq <- d_seq %>% pivot_longer(
+  cols = contains("resp_"), values_to = "obs", names_to = "resp",
+  names_transform = function(x) sub("^[^_]*_","",x) %>% as.numeric()
+) %>%
+  # add columns for median prediction, and lower and upper predictive interval boundaries
+  mutate( pred = NA, ci.low = NA, ci.upp = NA ) %>%
+  as.data.frame()
+
+# add predictions for each item/session/response combination
+for ( i in 1:nrow(d_seq) ) d_seq[ i , c("pred","ci.low","ci.upp") ] <- c(
+  sapply( 1:nrow(ppred), function(j) ( ppred[ j , with( df, which( item == d_seq$item[i] & post == d_seq$post[i] ) ) ] == d_seq$resp[i]+1 ) %>% sum() ) %>% median(),
+  sapply( 1:nrow(ppred), function(j) ( ppred[ j , with( df, which( item == d_seq$item[i] & post == d_seq$post[i] ) ) ] == d_seq$resp[i]+1 ) %>% sum() ) %>% ci(.95,"ETI") %>% as.data.frame() %>% select(CI_low) %>% as.numeric(),
+  sapply( 1:nrow(ppred), function(j) ( ppred[ j , with( df, which( item == d_seq$item[i] & post == d_seq$post[i] ) ) ] == d_seq$resp[i]+1 ) %>% sum() ) %>% ci(.95,"ETI") %>% as.data.frame() %>% select(CI_high) %>% as.numeric()
+)
+
+# plot it
+d_seq %>%
+  # shift such that zero-response frequencies are still visualized
+  mutate( obs = obs+1 ) %>%
+  # plotting proper
+  ggplot( aes( x = resp , y = obs , fill = Time ) ) +
+  # plot observed frequencies as bar plots
+  geom_bar( stat = "identity", position = position_dodge( width = .8 ), width = .7, alpha = .4 ) +
+  # plot predicted frequencies as points with whiskers
+  geom_pointrange(
+    with( d_seq, aes( x = resp, y = pred+1, ymin = ci.low+1, ymax = ci.upp+1, color = Time, fill = Time ) ),
+    shape = 21, size = 1.2, fatten = 1.5, position = position_dodge( width = .8 )
+    ) +
+  # wrap it
+  facet_wrap( ~ Item  , scales = "free" , nrow = 5 , ncol = 3 ) +
+  labs( x = "Response", y = "Frequency", fill = "", color = "" ) +
+  scale_fill_manual( values = cbPal[c(1,6)] ) +
+  scale_color_manual( values = cbPal[c(1,6)] ) +
+  scale_y_continuous( limits = c(0, 32), breaks = seq(1,31,5) , labels = seq(0,30,5) ) +
+  theme( legend.position = "bottom" )
+
+# save it
+ggsave( "figures/m1_teff_ppc_item_session.jpg", dpi = 600 )
+
+
+# ----------- posterior predictive check (composite score per session) -----------
+
+# prepare a table containing each item/session combination
+d_seq <- data.frame( post = 0:1 ) %>% mutate(
+  Time = factor( ifelse( post == 0 , "pre-surgery", "post-surgery"), levels = paste0(c("pre","post"),"-surgery"), ordered = T ),
+  `0` = c( ( df$resp[ which(df$post == 0) ] == 0 ) %>% sum(), ( df$resp[ which(df$post == 1) ] == 0 ) %>% sum() ),
+  `1` = c( ( df$resp[ which(df$post == 0) ] == 1 ) %>% sum(), ( df$resp[ which(df$post == 1) ] == 1 ) %>% sum() ),
+  `2` = c( ( df$resp[ which(df$post == 0) ] == 2 ) %>% sum(), ( df$resp[ which(df$post == 1) ] == 2 ) %>% sum() ),
+  `3` = c( ( df$resp[ which(df$post == 0) ] == 3 ) %>% sum(), ( df$resp[ which(df$post == 1) ] == 3 ) %>% sum() ),
+  `4` = c( ( df$resp[ which(df$post == 0) ] == 4 ) %>% sum(), ( df$resp[ which(df$post == 1) ] == 4 ) %>% sum() )
+) %>%
+  pivot_longer( cols = 3:7, values_to = "obs", names_to = "resp" ) %>%
+  # add columns for median prediction, and lower and upper predictive interval boundaries
+  mutate( resp = as.numeric(resp), pred = NA, ci.low = NA, ci.upp = NA ) %>% as.data.frame()
+                                               
+# add predictions for each item/session/response combination
+for ( i in 1:nrow(d_seq) ) d_seq[ i , c("pred","ci.low","ci.upp") ] <- c(
+  sapply( 1:nrow(ppred), function(j) ( ppred[ j , with( df, which( post == d_seq$post[i] ) ) ] == d_seq$resp[i]+1 ) %>% sum() ) %>% median(),
+  sapply( 1:nrow(ppred), function(j) ( ppred[ j , with( df, which( post == d_seq$post[i] ) ) ] == d_seq$resp[i]+1 ) %>% sum() ) %>% ci(.95,"ETI") %>% as.data.frame() %>% select(CI_low) %>% as.numeric(),
+  sapply( 1:nrow(ppred), function(j) ( ppred[ j , with( df, which( post == d_seq$post[i] ) ) ] == d_seq$resp[i]+1 ) %>% sum() ) %>% ci(.95,"ETI") %>% as.data.frame() %>% select(CI_high) %>% as.numeric()
+)
+
+# plot it
+d_seq %>% mutate( obs = obs+1 ) %>%
+  ggplot( aes( x = resp , y = obs , fill = Time ) ) +
+  geom_bar( stat = "identity", position = position_dodge( width = .8 ), width = .7, alpha = .4 ) +
+  geom_pointrange(
+    with( d_seq, aes( x = resp, y = pred+1, ymin = ci.low+1, ymax = ci.upp+1, color = Time, fill = Time ) ),
+    shape = 21, size = 2, fatten = 2.5, position = position_dodge( width = .8 )
+  ) +
+  facet_wrap( ~ Time  , scales = "free" , nrow = 2 , ncol = 1 ) +
+  labs( x = "Response", y = "Frequency", fill = "", color = "" ) +
+  scale_fill_manual( values = cbPal[c(1,6)] ) +
+  scale_color_manual( values = cbPal[c(1,6)] ) +
+  scale_y_continuous( limits = c(0, 322), breaks = seq(1,301,100) , labels = seq(0,300,100) ) +
+  theme( legend.position = "none" )
+
+# save it
+ggsave( "figures/m1_teff_ppc_comp_session.jpg", width = .75 * 9.65, height = .75 * 11.7, dpi = 600 )
+
+
+# ---- parameters description ----
+
+# plot the population-level parameters of the total effect of LEDD (i.e., m1_teff)
+m$m1_teff %>%
+  # extract parameter estimates
+  spread_draws( b_post1, b_led, `b_post1:led` ) %>%
+  pivot_longer( cols = contains("b_"), values_to = "est", names_to = "par" ) %>%
+  mutate( par = factor( par, levels = rev( paste0("b_",c("post1","led","post1:led") ) ), ordered = T ) ) %>%
+  # plot the values
+  ggplot( aes( y = par, x = est, fill = stat(abs(x) > .1) ) ) +
+  stat_halfeye( .width = .95, fatten_point = 3 ) +
+  geom_vline( xintercept = 0, linetype = "dashed" ) +
+  labs( x = "Effect size", y = "Prediction term" ) +
+  scale_y_discrete( labels = c("Post x LEDD", "LEDD", "Post-surgery") ) +
+  scale_x_continuous( breaks = seq(-.2,1,.2), labels = seq(-.2,1,.2) ) +
+  scale_fill_manual( values = c("gray80","skyblue") ) +
+  theme( legend.position = "none" )
+
+# save it
+ggsave( "figures/m1_teff_par_estimates.jpg", dpi = 600 )
+
+# extract posteriors of the b_post parameter from all three types of models and plot them
+sapply( names(m)[1:3], function(i) as_draws_df( m[[i]] ) %>% select(b_post1) ) %>%
+  cbind.data.frame() %>%
+  rename( "Vanilla" = "m0_meff.b_post1", "LEDD adjusted" = "m1_teff.b_post1", "Direct effect" = "m2_deff.b_post1") %>%
+  pivot_longer( everything(), names_to = "Model:", values_to = "Effect size" ) %>%
+  mutate( `Model:` = factor( `Model:`, levels = c("Vanilla","LEDD adjusted","Direct effect", ordered = T) ) ) %>%
+  # plotting proper
+  ggplot( aes(x = `Effect size`, color = `Model:`, fill = `Model:` ) ) +
+  stat_slab( geom = "slab", linetype = "solid", size = 2 ) +
+  geom_vline( xintercept = 0, linetype = "dashed" ) +
+  labs( x = "Effect size", y = "Density", title = "Post-minus-pre-surgery IADL (standardized effect)" ) +
+  scale_y_continuous( breaks = seq(0,1,.2), labels = seq(0,1,.2) %>% sprintf("%.1f",.) ) +
+  scale_x_continuous( breaks = seq(-.2,1.2,.2), labels = seq(-.2,1.2,.2) %>% sprintf("%.1f",.) ) +
+  scale_color_manual( values = alpha( cbPal[c(5,3,4)], 1 ) ) +
+  scale_fill_manual( values = alpha( cbPal[c(5,3,4)], .1 ) ) +
+  theme( legend.position = "bottom", plot.title = element_text(hjust = .5) )
+
+# save it
+ggsave( "figures/b_post1_estimates.jpg", dpi = 600 )
+
+
+
+# next add conditional_effects plot 
---
+++ models/m0_meff.stan
@@ -0,0 +1,123 @@
+// generated with brms 2.17.0
+functions {
+  /* cumulative-probit log-PDF for a single response
+   * Args:
+   *   y: response category
+   *   mu: latent mean parameter
+   *   disc: discrimination parameter
+   *   thres: ordinal thresholds
+   * Returns:
+   *   a scalar to be added to the log posterior
+   */
+   real cumulative_probit_lpmf(int y, real mu, real disc, vector thres) {
+     int nthres = num_elements(thres);
+     real p;
+     if (y == 1) {
+       p = Phi(disc * (thres[1] - mu));
+     } else if (y == nthres + 1) {
+       p = 1 - Phi(disc * (thres[nthres] - mu));
+     } else {
+       p = Phi(disc * (thres[y] - mu)) -
+           Phi(disc * (thres[y - 1] - mu));
+     }
+     return log(p);
+   }
+  /* cumulative-probit log-PDF for a single response and merged thresholds
+   * Args:
+   *   y: response category
+   *   mu: latent mean parameter
+   *   disc: discrimination parameter
+   *   thres: vector of merged ordinal thresholds
+   *   j: start and end index for the applid threshold within 'thres'
+   * Returns:
+   *   a scalar to be added to the log posterior
+   */
+   real cumulative_probit_merged_lpmf(int y, real mu, real disc, vector thres, int[] j) {
+     return cumulative_probit_lpmf(y | mu, disc, thres[j[1]:j[2]]);
+   }
+}
+data {
+  int<lower=1> N;  // total number of observations
+  int Y[N];  // response variable
+  int<lower=2> nthres;  // number of thresholds
+  int<lower=1> K;  // number of population-level effects
+  matrix[N, K] X;  // population-level design matrix
+  // data for group-level effects of ID 1
+  int<lower=1> N_1;  // number of grouping levels
+  int<lower=1> M_1;  // number of coefficients per level
+  int<lower=1> J_1[N];  // grouping indicator per observation
+  // group-level predictor values
+  vector[N] Z_1_1;
+  // data for group-level effects of ID 2
+  int<lower=1> N_2;  // number of grouping levels
+  int<lower=1> M_2;  // number of coefficients per level
+  int<lower=1> J_2[N];  // grouping indicator per observation
+  // group-level predictor values
+  vector[N] Z_2_1;
+  int prior_only;  // should the likelihood be ignored?
+}
+transformed data {
+  int Kc = K;
+  matrix[N, Kc] Xc;  // centered version of X
+  vector[Kc] means_X;  // column means of X before centering
+  for (i in 1:K) {
+    means_X[i] = mean(X[, i]);
+    Xc[, i] = X[, i] - means_X[i];
+  }
+}
+parameters {
+  vector[Kc] b;  // population-level effects
+  ordered[nthres] Intercept;  // temporary thresholds for centered predictors
+  vector<lower=0>[M_1] sd_1;  // group-level standard deviations
+  vector[N_1] z_1[M_1];  // standardized group-level effects
+  vector<lower=0>[M_2] sd_2;  // group-level standard deviations
+  vector[N_2] z_2[M_2];  // standardized group-level effects
+}
+transformed parameters {
+  real disc = 1;  // discrimination parameters
+  vector[N_1] r_1_1;  // actual group-level effects
+  vector[N_2] r_2_1;  // actual group-level effects
+  real lprior = 0;  // prior contributions to the log posterior
+  r_1_1 = (sd_1[1] * (z_1[1]));
+  r_2_1 = (sd_2[1] * (z_2[1]));
+  lprior += normal_lpdf(b | 0, 0.5);
+  lprior += student_t_lpdf(Intercept | 3, 0, 2.5);
+  lprior += student_t_lpdf(sd_1 | 3, 0, 2.5)
+    - 1 * student_t_lccdf(0 | 3, 0, 2.5);
+  lprior += student_t_lpdf(sd_2 | 3, 0, 2.5)
+    - 1 * student_t_lccdf(0 | 3, 0, 2.5);
+}
+model {
+  // likelihood including constants
+  if (!prior_only) {
+    // initialize linear predictor term
+    vector[N] mu = Xc * b;
+    for (n in 1:N) {
+      // add more terms to the linear predictor
+      mu[n] += r_1_1[J_1[n]] * Z_1_1[n] + r_2_1[J_2[n]] * Z_2_1[n];
+    }
+    for (n in 1:N) {
+      target += cumulative_probit_lpmf(Y[n] | mu[n], disc, Intercept);
+    }
+  }
+  // priors including constants
+  target += lprior;
+  target += std_normal_lpdf(z_1[1]);
+  target += std_normal_lpdf(z_2[1]);
+}
+generated quantities {
+  // compute actual thresholds
+  vector[nthres] b_Intercept = Intercept + dot_product(means_X, b);
+  // additionally sample draws from priors
+  real prior_b = normal_rng(0,0.5);
+  real prior_Intercept = student_t_rng(3,0,2.5);
+  real prior_sd_1 = student_t_rng(3,0,2.5);
+  real prior_sd_2 = student_t_rng(3,0,2.5);
+  // use rejection sampling for truncated priors
+  while (prior_sd_1 < 0) {
+    prior_sd_1 = student_t_rng(3,0,2.5);
+  }
+  while (prior_sd_2 < 0) {
+    prior_sd_2 = student_t_rng(3,0,2.5);
+  }
+}
