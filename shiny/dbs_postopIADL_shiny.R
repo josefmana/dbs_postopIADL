d0fe651120865a31035c7c4e4688d4dcf8ac48fd
josefmana <josef.mana@protonmail.com>
2022-10-24 22:17:37 +0200

    shiny v1.1 contrasts added

--- shiny/dbs_postopIADL_shiny.R
+++ shiny/dbs_postopIADL_shiny.R
@@ -35,27 +35,32 @@ cbPal <- c( "#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#
 
 ui <- fluidPage(
   
-  sidebarLayout(
+  # top row
+  fluidRow(
     
-    # Inputs: select LEDD to predict at
-    sidebarPanel(
-      
-      # Select input for the pre-surgery LEDD
-      sliderInput( inputId = "led_0", label = "Pre-surgery LEDD (mg)", min = 0, max = 5000, value = 1820, step = 1),
-      numericInput( inputId = "led_n0", label = NULL, min = 0, max = 5000, value = 1820),
-      
-      # Select input for the post-surgery LEDD
-      sliderInput( inputId = "led_1", label = "Post-surgery LEDD (mg)", min = 0, max = 5000, value = 833, step = 1),
-      numericInput( inputId = "led_n1", label = NULL, min = 0, max = 5000, value = 833 )
-      
+    # inputs: select LEDD to predict at
+    column( width = 6,
+            
+            # select input for the pre-surgery LEDD
+            sliderInput( inputId = "led_0", label = "Pre-surgery LEDD (mg)", min = 0, max = 5000, value = 1820, step = 1),
+            numericInput( inputId = "led_n0", label = NULL, min = 0, max = 5000, value = 1820),
+            
+            # select input for the post-surgery LEDD
+            sliderInput( inputId = "led_1", label = "Post-surgery LEDD (mg)", min = 0, max = 5000, value = 833, step = 1),
+            numericInput( inputId = "led_n1", label = NULL, min = 0, max = 5000, value = 833 )
+            
+            ),
+    
+    # outputs: contrasts plot
+    column( width = 6, plotOutput( outputId = "con_plt" ) )
+    
     ),
   
-    # Output: Show the plot
-    mainPanel(
-      plotOutput( outputId = "pred_plt", height = 900 )
-      )
-    
-  )
+  # bottom row
+  hr(),
+  
+  # outputs: raw predictions
+  plotOutput( outputId = "pred_plt" )
   
 )
 
@@ -90,16 +95,36 @@ server <- function(input, output) {
         # plot it
         ggplot( aes( x = resp, y = Md, ymin = CI.low, ymax = CI.upp, color = `Session:`, fill = `Session:` ) ) +
         geom_pointrange( shape = 21, size = 3.5, fatten = 2, position = position_dodge( width = .25 ) ) +
-        labs( x = element_blank(), y = "Probability of response (%)" ) +
+        labs( x = "Difficulties with instrumental activities of daily living (IADLs)", y = "Probability of response (%)" ) +
         scale_x_discrete( labels = c('"cannot do"','"a lot"', '"somewhat"','"a little"','"none"') ) +
         scale_y_continuous( limits = c(0,100), breaks = seq(0,100,25), labels = seq(0,100,25) ) +
         scale_color_manual( values = cbPal[c(1,3)] ) +
         scale_fill_manual( values = c("black",cbPal[6]) ) +
-        coord_flip() +
         theme_minimal( base_size = 18 ) +
-        theme( legend.position = "bottom" )
+        theme( legend.position = "bottom", axis.title.x = element_text( vjust = -1 ) )
     }
   )
+  
+  # plot contrasts
+  output$con_plt <- renderPlot(
+    { data.frame( resp = rep(1:5) ) %>% # prepare data
+        # add median and boundary (95% HDI) contrasts
+        mutate( Md = 100 * sapply( 1:nrow(.), function(i) median( ( ppred()[,2,resp[i]] - ppred()[,1,resp[i]] ) ) ),
+                CI.low = 100 * sapply( 1:nrow(.), function(i) ci( ( ppred()[,2,resp[i]] - ppred()[,1,resp[i]] ), method = "HDI", ci = .95 ) %>% as.data.frame() %>% select(CI_low) %>% as.numeric() ),
+                CI.upp = 100 * sapply( 1:nrow(.), function(i) ci( ( ppred()[,2,resp[i]] - ppred()[,1,resp[i]] ), method = "HDI", ci = .95 ) %>% as.data.frame() %>% select(CI_high) %>% as.numeric() ),
+                resp = as.factor(resp) ) %>%
+        # plot it
+        ggplot( aes( x = resp, y = Md, ymin = CI.low, ymax = CI.upp ) ) +
+        geom_pointrange( shape = 21, size = 3.5, fatten = 2, color = cbPal[2], fill = cbPal[7] ) +
+        geom_hline( yintercept = 0, size = 2/3, color = "black" ) +
+        labs( x = "Difficulties with IADLs", y = "Contrast (post-minus-pre-surgery, %)" ) +
+        scale_x_discrete( labels = c('"cannot do"','"a lot"', '"somewhat"','"a little"','"none"') ) +
+        scale_y_continuous( breaks = seq(-50,50,5), labels = seq(-50,50,5) ) +
+        theme_minimal( base_size = 18 ) +
+        coord_flip()
+    }
+  )
+  
 }
 
 
